6f0466a8591dfe2c386660f133e192ee88fd8445
kempo <kemal.aslan@hackuva.de>
2022-01-10 14:50:25 +0100

    website

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,108 @@
     <section id="hero">
       <div class="hero-container">
         <h1>Trockentrenntoilette</h1>
-        <h2>Autarkt - Nachhaltig - Umweltbewusst - Die Trockentrenntoilette</h2>
+        <h2>Anbieter, Produkte & Testberichte</h2>
         <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
       </div>
     </section><!-- End Hero -->
 
     <main id="main">
+
+      <!-- ======= About Me Section ======= -->
+      <section id="about" class="about">
+        <div class="container">
+
+          <div class="section-title">
+            <span>Anbieter</span>
+            <h2>TROBOLO</h2>
+            <p>Autark, nachhaltig und umweltbewusst</p>
+          </div>
+
+          <div class="row">
+            <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+            <div class="col-lg-8 d-flex flex-column align-items-stretch">
+              <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+                <div class="row">
+                  <div class="col-lg-6">
+                    <ul>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Marke:</strong> <span>TROBOLO</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Webseite:</strong> <span>www.trobolo.com</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Telefon:</strong> <span>+49 511 / 51510816</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Firmensitz:</strong> <span>Hannover, Deutschland</span></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6">
+                    <ul>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
+                      <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="row mt-n4">
+                  <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                    <div class="count-box">
+                      <i class="bi bi-emoji-smile" style="color: #20b38e;"></i>
+                      <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+                      <p><strong>Happy Clients</strong> consequuntur voluptas nostrum aliquid ipsam architecto ut.</p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                    <div class="count-box">
+                      <i class="bi bi-journal-richtextr" style="color: #8a1ac2;"></i>
+                      <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
+                      <p><strong>Projects</strong> adipisci atque cum quia aspernatur totam laudantium et quia dere tan</p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                    <div class="count-box">
+                      <i class="bi bi-clock" style="color: #2cbdee;"></i>
+                      <span data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1" class="purecounter"></span>
+                      <p><strong>Years of experience</strong> aut commodi quaerat modi aliquam nam ducimus aut voluptate non vel</p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                    <div class="count-box">
+                      <i class="bi bi-award" style="color: #ffb459;"></i>
+                      <span data-purecounter-start="0" data-purecounter-end="16" data-purecounter-duration="1" class="purecounter"></span>
+                      <p><strong>Awards</strong> rerum asperiores dolor alias quo reprehenderit eum et nemo pad der</p>
+                    </div>
+                  </div>
+                </div>
+              </div><!-- End .content-->
+
+              <div class="skills-content ps-lg-4">
+                <div class="progress">
+                  <span class="skill">HTML <i class="val">100%</i></span>
+                  <div class="progress-bar-wrap">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
+                <div class="progress">
+                  <span class="skill">CSS <i class="val">90%</i></span>
+                  <div class="progress-bar-wrap">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
+                <div class="progress">
+                  <span class="skill">JavaScript <i class="val">75%</i></span>
+                  <div class="progress-bar-wrap">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </section><!-- End About Me Section -->
 
       <!-- ======= About Me Section ======= -->
       <section id="about" class="about">
