b561f6a085c7da5857cc946c8fa2b5aa9b0bda99
trockentrenntoilette <trockentrenntoilette@protonmail.com>
2022-09-18 11:47:07 +0200

    deploying

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@/modules/sitemapRouteGenerator'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
